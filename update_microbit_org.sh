54d86685f8fdefc83ff846c2e41bc04732311141
Sam Kent <sam@microbit.org>
2018-11-12 12:02:25 +0000

    Add script to update microbit_org branch

---
+++ update_microbit_org.sh
@@ -0,0 +1,2 @@
+git checkout microbit_org
+git checkout master AUTHORS LICENSE ace blockly editor.html lzma tests CHANGELOG README.rst base64-js firmware.hex microbit_blocks tests.html CONTRIBUTING.rst VERSION help.html python-main.js static .gitmodules
